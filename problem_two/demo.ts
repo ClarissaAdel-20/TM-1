4f6d3b952ba55193f3dae2564e2b9e0bdb098797
ClarissaAdel-20 <clarislynn20@gmail.com>
2026-04-10 21:21:24 +0700

    third commit

--- problem_two/demo.ts
+++ problem_two/demo.ts
@@ -1,23 +1,5 @@
-import { CreatureSystem } from "./CreatureSystem";
-import { DiverSystem } from "./DiverSystem";
-import { EnvironmentSystem } from "./EnvironmentSystem";
+import { DiveFacade } from "./facade";
 
-const environment = new EnvironmentSystem();
-const creatures = new CreatureSystem();
-const diver = new DiverSystem();
-
-console.log("\nPreparing dive manually...\n");
-
-environment.initialize();
-environment.loadTerrain();
-environment.setWaterConditions();
-
-creatures.initialize();
-creatures.spawnCreatures();
-creatures.assignBehaviors();
-
-diver.initialize();
-diver.equipGear();
-diver.checkVitals();
-
+// cukup 1 baris aja
+const dive = new DiveFacade();
 console.log("\nDive is ready, but setup is complex and repetitive.\n");
---
+++ problem_two/facade.ts
@@ -0,0 +1,29 @@
+import { CreatureSystem } from "./CreatureSystem";
+import { DiverSystem } from "./DiverSystem";
+import { EnvironmentSystem } from "./EnvironmentSystem";
+
+export class DiveFacade{
+    private environment = new EnvironmentSystem();
+    private creatures = new CreatureSystem();
+    private diver = new DiverSystem();
+
+    startDive(){
+        console.log("\nPreparing dive atutomatically...\n");
+
+        this.environment.initialize();
+        this.environment.loadTerrain();
+        this.environment.setWaterConditions();
+
+        this.creatures.initialize();
+        this.creatures.loadTerrain();
+        this.creatures.setWaterConditions();
+
+        this.diver.initialize();
+        this.diver.equipGear();
+        this.diver.checkVitals();
+
+        console.log("\n Dive is ready! \n");
+
+        
+    }
+}
